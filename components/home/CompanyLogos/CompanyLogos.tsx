396cd7574c1596084924dd7a74feb1f3890acab7
olcayhan <71206063+olcayhan@users.noreply.github.com>
2026-02-16 15:16:45 +0300

    feat: Update and expand the displayed company logos and adjust their container and image styling.

--- components/home/CompanyLogos/CompanyLogos.tsx
+++ components/home/CompanyLogos/CompanyLogos.tsx
@@ -1,43 +1,43 @@
 'use client';
 
-import React from 'react';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
 
 const companies = [
-    { name: 'Acibadem', logo: 'https://companieslogo.com/img/orig/ACIBADEM.IS-d8777174.png?t=1720244490' },
-    { name: 'Akbank', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Akbank_logo.svg/1200px-Akbank_logo.svg.png' },
-    { name: 'Akcansa', logo: 'https://companieslogo.com/img/orig/AKCNS.IS-7467389a.png?t=1720244490' },
-    { name: 'Akkim', logo: 'https://www.akkim.com/wp-content/uploads/2021/01/akkim-logo.png' },
-    { name: 'Akkok', logo: 'https://upload.wikimedia.org/wikipedia/tr/6/6f/Akk%C3%B6k_Holding_logo.png' },
-    { name: 'Aktif Bank', logo: 'https://upload.wikimedia.org/wikipedia/commons/4/4e/Aktif_Bank_logo.png' },
-    { name: 'Allianz', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Allianz_logo.svg/2560px-Allianz_logo.svg.png' },
-    // Add more logos as needed
+    { name: 'Acibadem', logo: 'https://upload.wikimedia.org/wikipedia/commons/f/ff/Ac%C4%B1badem_Grup_logo.svg' },
+    { name: 'Akbank', logo: 'https://upload.wikimedia.org/wikipedia/commons/7/7b/Akbank_logo.svg' },
+    { name: 'Akcansa', logo: 'https://www.akcansa.com.tr/wp-content/uploads/2023/05/logo-akcansa.svg' },
+    { name: 'Akkim', logo: 'https://akkim.com.tr/wp-content/uploads/2022/03/Akkim-logo-1.png' },
+    { name: 'Akkok', logo: 'https://upload.wikimedia.org/wikipedia/commons/5/52/Akk%C3%B6k_Holding_logo.png' },
+    { name: 'Aktif Bank', logo: 'https://upload.wikimedia.org/wikipedia/commons/e/e9/Aktif_Bank_logo.svg' },
+    { name: 'Allianz', logo: 'https://upload.wikimedia.org/wikipedia/commons/4/4b/Allianz.svg' },
+    { name: 'Anadolu Sigorta', logo: 'https://upload.wikimedia.org/wikipedia/commons/9/9e/Anadolu_Sigorta_logo.svg' },
+    { name: 'Anel Group', logo: 'https://www.etmd.org.tr/wp-content/uploads/2020/01/20-ANEL-logo.png' },
+    { name: 'Arçelik', logo: 'https://upload.wikimedia.org/wikipedia/commons/b/bd/Ar%C3%A7elik_logo.svg' },
+    { name: 'Bosch', logo: 'https://upload.wikimedia.org/wikipedia/commons/2/23/BSH_Bosch_und_Siemens_Hausger%C3%A4te_logo.svg' },
+    { name: 'Bemzi Alem', logo: 'https://upload.wikimedia.org/wikipedia/tr/2/21/Bezmi%C3%A2lem_Vak%C4%B1f_%C3%9Cniversitesi_logosu.png' },
+    { name: 'Rönesans Holding', logo: 'https://upload.wikimedia.org/wikipedia/tr/d/d7/Ronesans_holding.png' },
+    { name: 'TAV Airport', logo: 'https://tavairports.com/images/TAV-Havalimanlari-logo.svg' }
 ];
 
 export default function CompanyLogos() {
     return (
         <Box sx={{ py: { xs: 4, sm: 6 } }}>
-            <Container maxWidth="lg">
+            <Container maxWidth="lg" sx={{ backgroundColor: '#f7f6f6', borderRadius: '20px', paddingY: '30px', paddingX: '18px' }}>
                 <Grid container spacing={{ xs: 2, sm: 4 }} alignItems="center" justifyContent="center">
                     {companies.map((company) => (
-                        <Grid key={company.name} size={{ xs: 6, sm: 4, md: 2 }} sx={{ textAlign: 'center' }}>
+                        <Grid key={company.name} columns={{ xs: 7, sm: 4, md: 2 }} sx={{ textAlign: 'center' }}>
                             <Box
                                 component="img"
                                 src={company.logo}
                                 alt={company.name}
                                 sx={{
-                                    maxWidth: '100px',
-                                    maxHeight: '50px',
-                                    opacity: 0.6,
                                     filter: 'grayscale(100%)',
-                                    transition: '0.3s',
-                                    '&:hover': {
-                                        opacity: 1,
-                                        filter: 'grayscale(0%)'
-                                    }
+                                    opacity: 0.6,
+                                    objectFit: 'contain',
+                                    width: '136px',
+                                    height: '95px'
                                 }}
                             />
                         </Grid>
